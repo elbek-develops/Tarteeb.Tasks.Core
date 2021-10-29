05ad97a681ecb070d51bae5a1462e31ca49df789
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2021-10-29 04:47:25 -0700

    Initialize basic structure

---
+++ Tarteeb.Tasks.Core/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Tarteeb.Tasks.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => Ok("Hey, I am up and running");
+    }
+}
